48c99230c404d60511aad78e38b4d2edf3891f0e
antonioluppi-GH <83036973+antonioluppi-GH@users.noreply.github.com>
2021-07-12 11:59:32 -0300

    Ejercicios faltantes
    Prof 2 y 3 Numpy

--- ejercicios_profundizacion/profundizacion_3.py
+++ ejercicios_profundizacion/profundizacion_3.py
@@ -62,5 +62,10 @@ if __name__ == '__main__':
     # A partir de aquí escriba el código que resuelve el enunciado
     # Leer el enunciado con atención y consultar cualquier duda
 
+    
+    lista_compra_productos = [producto.get(x) if x in producto
+                             else 'Nan' for x in lista_compra_id]
+    print(lista_compra_productos)
+
 
     print("terminamos")
